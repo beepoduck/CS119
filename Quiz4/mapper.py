ffd96b6d02efcfdffd278da2e02fe7625178220f
beepoduck <ash.sze@tufts.edu>
2024-02-26 12:05:44 -0500

    problem2

---
+++ Quiz4/mapper.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys, re
+import os
+import string
+library = []
+
+def clean_text(text):
+    text = str(text).lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
+    text = re.sub('[\d\n]', ' ', text)
+    return text
+
+
+def calc_Valence(text):
+    cleaned = clean_text(text)
+    words = cleaned.split()
+    for word in words:
+        for i in range (len(library)):
+            if (library[i]["word"] == word):
+                filename = os.environ['mapreduce_map_input_file']
+                key = filename.split("hdfs://cluster-119/user/ashysze/quiz5/prez_speeches/")[1].split(".tar.gz")[0]
+                value = library[i]["value"]
+                print(key,"\t",value)
+
+
+def main(argv):
+    line = sys.stdin.readline()
+    with open('AFINN.txt', 'r') as file:
+        for line in file:
+            words = line.split()
+            dict = {
+                "word": words[0],
+                "value": words[1] }
+            library.append(dict)
+        # print(library)
+    
+    try:
+        while line:
+            calc_Valence(line)
+            line = sys.stdin.readline()
+    except EOFError as error:
+        return None
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ Quiz4/reducer.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""reducer.py"""
+
+from operator import itemgetter
+import sys
+
+current_word = None
+current_totalValence = 0
+word = None
+count = 0
+totalvalence = 0
+
+# input comes from STDIN
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+
+    # parse the input we got from mapper.py
+    word, valence = line.split('\t', 1)
+
+    # convert count (currently a string) to int
+    try:
+        valence = float(valence)
+    except ValueError:
+        # count was not a number, so silently
+        # ignore/discard this line
+        continue
+    # this IF-switch only works because Hadoop sorts map output
+    # by key (here: word) before it is passed to the reducer
+    if current_word == word:
+        current_totalValence += valence
+        count += 1.0
+        # print("Valence is", valence, "count is", count)
+    else:
+        if current_word:
+            # write result to STDOUT
+            average = float(current_totalValence/count)
+            print ('%s\t%s' % (current_word, average))
+            
+        # print("Reset for next word")
+        count = 1
+        current_word = word
+        current_totalValence = valence
+        # print(current_word, current_totalValence)
+    
+    
+# # do not forget to output the last word if needed!
+if current_word == word:
+    average = current_totalValence/count
+    print ('%s\t%s' % (current_word, average))
